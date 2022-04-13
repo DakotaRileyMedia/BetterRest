0bd421de416dd50aa488a34c456f95c844f14377
DakotaRileyMedia <dakotacustompcs@gmail.com>
2022-04-12 23:47:07 -0400

    App Completed

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -2,15 +2,73 @@
 //  ContentView.swift
 //  BetterRest
 //
-//  Created by Dakota Riley on 4/12/22.
+//  Created by Dakota Riley on 4/10/22.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+    
+    var sleepResults: String {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            return "Your ideal bedtime is… \(sleepTime.formatted(date: .omitted, time: .shortened))"
+        } catch {
+            return "Sorry, there was a problem calculating your bedtime."
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                } header: {
+                    Text("When do you want to wake up?")
+                }
+                
+                Section {
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                } header: {
+                    Text("Desired amount of sleep")
+                }
+                
+                Section {
+                    Picker(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", selection: $coffeeAmount) {
+                        ForEach(1..<21) {
+                            Text($0-1, format: .number)
+                        }
+                    }
+                } header: {
+                    Text("Daily coffee intake")
+                }
+                
+                Text(sleepResults)
+                    .font(.title3)
+            }
+            .navigationTitle("BetterRest")
+        }
     }
 }
 
