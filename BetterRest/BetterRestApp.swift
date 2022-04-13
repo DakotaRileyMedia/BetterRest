bbc6bae52def03e1e0d3fed0924de3d30aef4225
DakotaRileyMedia <dakotacustompcs@gmail.com>
2022-04-12 23:39:38 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Dakota Riley on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
